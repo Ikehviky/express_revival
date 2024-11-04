8222cc9af2dc298469f3e8862202848bda199c7b
somtooffor <somto.offor@opexconsult.co.uk>
2024-11-04 14:17:57 +0100

    implemented middleware, controllers, routes and server

---
+++ routes/posts.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { createPost, deletePost, getPost, getPosts, updatePost } from '../controllers/postController.js'
+const router = express.Router();
+
+// Get all posts
+router.get('/', getPosts);
+
+// Get a posts
+router.get('/:id', getPost);
+
+// Create new post
+router.post('/', createPost);
+
+// Update post
+router.put('/:id', updatePost);
+
+//Delete post
+router.delete('/:id', deletePost);
+
+export default router;
